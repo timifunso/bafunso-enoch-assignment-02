587a13ebeb150c1f7dfd43a6f06d8e4c172e3085
Enoch Bafunso <109138477+timifunso@users.noreply.github.com>
2022-07-15 20:55:09 +0000

    check challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -14,6 +14,16 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
+    let sum = 0;
+    for (let i = 0; i <arrayOfNumbers.length; i ++){
+        sum += arrayOfNumbers[i]
+    
+
+    }
+}
+ assignment.sumOfNumbers = sumOfNumbers;
+
+function sumOfNumbers(arrayOfNumbers) {
     return arrayOfNumbers.length;
 }
 // assignment.sumOfNumbers = sumOfNumbers;
@@ -28,9 +38,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evens = 0
+
+    for (i = 0; i < arrayOfNumbers.length; i++){
+        if(arrayOfNumbers[i]  % 2 === 0){
+            evens++;
+
+        }
+
+    }
+    return evens
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
+
 
 /**
  * Challenge - 3
